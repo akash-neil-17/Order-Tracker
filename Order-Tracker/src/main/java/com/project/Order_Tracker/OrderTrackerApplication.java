852d8c2eff3967e8cfc5e9c78d8331762ae94e34
Akash Chakraborty <chakraborty.akash@sonaliintellect.com>
2024-09-12 13:55:38 +0600

    Project has been created

---
+++ Order-Tracker/src/main/java/com/project/Order_Tracker/OrderTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.project.Order_Tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderTrackerApplication.class, args);
+	}
+
+}
